2fff5afaea90cb3a4bbac69712935c94d47f810c
Beaugrand, Kevin <kevin.beaugrand@cgi.com>
2020-10-07 09:17:52 +0200

    Move to Arduino Library

--- inc/IoTDevice.cpp
+++
@@ -1,9 +0,0 @@
-#include <WiFi.h>
-
-#include <AzureIoTHub.h>
-#include <apps/sntp/sntp.h>
-
-#include "blink.h"
-#include "device_configuration.h"
-#include "iot_device_connection.h"
-#include "iot_device_d2c.h"
---
+++ src/ESP32AzureIoTOTA.cpp
@@ -0,0 +1,9 @@
+#include <WiFi.h>
+
+#include <AzureIoTHub.h>
+#include <apps/sntp/sntp.h>
+
+#include "core/blink.h"
+#include "core/device_configuration.h"
+#include "core/iot_device_connection.h"
+#include "core/iot_device_d2c.h"
